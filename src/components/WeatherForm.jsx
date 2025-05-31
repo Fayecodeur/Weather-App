d211748f90bd5e04ea51fa574681e575b74f645b
Fayecodeur <fayecodeur99@gmail.com>
2025-05-31 18:04:46 +0000

    Gestion des erreurs

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function WeatherForm({ handleSearchCity }) {
   const [city, setCity] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    handleSearchCity(city);
-    setCity("");
-  };
 
-  const handleChangeInput = (e) => {
-    const value = e.target.value;
-    setCity(value);
+    if (!city.trim()) {
+      toast.error("Veuillez entrer un nom de ville.");
+      return;
+    }
+
+    const success = await handleSearchCity(city.trim());
+    if (!success) {
+      toast.error("Ville introuvable.");
+    } else {
+      setCity("");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={city} onChange={handleChangeInput} />
-      <button onClick={handleSubmit} type="submit">
-        Rechercher
-      </button>
-    </form>
+    <>
+      <Toaster position="top-center" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Entrez une ville"
+        />
+        <button type="submit">Rechercher</button>
+      </form>
+    </>
   );
 }
